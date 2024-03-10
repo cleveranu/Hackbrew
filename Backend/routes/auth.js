620ecfcf8aa96ea41d7bf63fab440151fb1f611b
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2024-03-10 08:32:20 +0530

    backend changes for login

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -20,8 +20,8 @@ router.post("/", async (req, res) => {
     if (!validPassword)
       return res.status(401).send({ message: "Invalid Email or Password" });
 
-    // const token = user.generateAuthToken();
-    res.status(200).send({ data: null, message: "logged in successfully" });
+    const token = user.generateAuthToken();
+    res.status(200).send({ data: token, message: "logged in successfully" });
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
   }
