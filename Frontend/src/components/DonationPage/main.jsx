d7be49310253cad8a4997fa85b9031c203ceaecc
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2024-03-10 08:12:12 +0530

    ngo signup

--- Frontend/src/components/DonationPage/main.jsx
+++ Frontend/src/components/DonationPage/main.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import Navigation from './Navbar';
-import Products from './Product';
-import products from '../../../../Backend/data';
-import Recommended from './Recommended';
-import { Link } from 'react-router-dom';
-import './Card.css'; // Assuming CSS for Card is stored here
+import React, { useState } from "react";
+import Navigation from "./Navbar";
+import Products from "./Product";
+import products from "../data";
+import Recommended from "./Recommended";
+import { Link } from "react-router-dom";
+import "./Card.css"; // Assuming CSS for Card is stored here
 
 function Main() {
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  const filteredItems = products.filter(
-    (product) => product.Name.toLowerCase().includes(query.toLowerCase())
+  const filteredItems = products.filter((product) =>
+    product.Name.toLowerCase().includes(query.toLowerCase())
   );
 
   const handleChange = (event) => {
@@ -44,34 +44,34 @@ function Main() {
       );
     }
 
-    return filteredProducts.map(({  id,
-      Name,
-      One_line,
-      Description,
-      Need_Rating,
-      Established_in,
-      State,
-      City }) => (
-      <Link 
-      to={`/product-detail/${id}`} 
-    
-      className="card-link" key={id}>
-      <div className="card">
-        {/* <img src={img} alt={Name} className="card-image"/> */}
-        <div className="card-content">
-          <h3 className="card-title">{Name}</h3>
-          <div className="card-rating">{Need_Rating}Need Stars</div>
-          <div className="card-reviews">{One_line} Desc:</div>
-          <div className="card-price">
-            <span className="card-prevPrice">{State}</span>
-            <span className="card-newPrice">{City}</span>
+    return filteredProducts.map(
+      ({
+        id,
+        Name,
+        One_line,
+        Description,
+        Need_Rating,
+        Established_in,
+        State,
+        City,
+      }) => (
+        <Link to={`/product-detail/${id}`} className="card-link" key={id}>
+          <div className="card">
+            {/* <img src={img} alt={Name} className="card-image"/> */}
+            <div className="card-content">
+              <h3 className="card-title">{Name}</h3>
+              <div className="card-rating">{Need_Rating}Need Stars</div>
+              <div className="card-reviews">{One_line} Desc:</div>
+              <div className="card-price">
+                <span className="card-prevPrice">{State}</span>
+                <span className="card-newPrice">{City}</span>
+              </div>
+              <button className="card-buy-button">Visit</button>
+            </div>
           </div>
-          <button className="card-buy-button">Visit</button>
-        </div>
-      </div>
-    </Link>
-    
-    ));
+        </Link>
+      )
+    );
   }
 
   const result = filteredData(products, selectedCategory, query);
